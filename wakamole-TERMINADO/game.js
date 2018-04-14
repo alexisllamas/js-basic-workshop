d548e5c006a98ba6560b5d13e036ff88b0570dc5
Alexis Navarro <alexisnavarro23@gmail.com>
2018-04-14 05:04:03 +0000

    format javascript

--- wakamole-TERMINADO/game.js
+++ wakamole-TERMINADO/game.js
@@ -1,22 +1,27 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+const button = document.querySelectorAll('#start');
+
 let lastHole;
 let timeUp = false;
 let score = 0;
+
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
+
 function randomHole(holes) {
   const idx = Math.floor(Math.random() * holes.length);
   const hole = holes[idx];
   if (hole === lastHole) {
-    console.log('Ah nah thats the same one bud');
+    console.log('Nope, no se vale repetir');
     return randomHole(holes);
   }
   lastHole = hole;
   return hole;
 }
+
 function peep() {
   const time = randomTime(500, 2000);
   const hole = randomHole(holes);
@@ -26,6 +31,7 @@ function peep() {
     if (!timeUp) peep();
   }, time);
 }
+
 function startGame() {
   scoreBoard.textContent = 0;
   timeUp = false;
@@ -33,11 +39,14 @@ function startGame() {
   peep();
   setTimeout(() => timeUp = true, 10000)
 }
-document.getElementById('start').addEventListener('click', startGame)
+
+
 function bonk(e) {
-  if(!e.isTrusted) return; // cheater!
+  if(!e.isTrusted) return; // tramposo
   score++;
   this.parentNode.classList.remove('up');
   scoreBoard.textContent = score;
 }
+
+document.getElementById('start').addEventListener('click', startGame);
 moles.forEach(mole => mole.addEventListener('click', bonk));
